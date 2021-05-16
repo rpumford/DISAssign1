683ca1089486e668594985b4f5abcbf355e3b3af
Ryan Pumford <rpumford@gmail.com>
2021-05-16 18:04:32 -0400

    Set up assignment tempmlate

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,9 +1,61 @@
 ﻿using System;
 
-namespace Assignment1 {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test code to call the first method and make sure it works.
+            int[] test = targetRange(new int[] { 2, 3 }, 3);
+            Console.WriteLine("Output: " + test[0]);
         }
+
+        public static int[] targetRange(int[] marks, int target)
+        {
+            /*Q1: Professor Agrawal receives an array of integer points sorted in ascending order, the task is to find the initial and final index of a given target point’s value.
+            If the target point value is not found in the array of integers, return [-1,-1]
+            The professor had to leave for a conference at short notice and asked you to complete the task for him. He instructed you to limit the time complexity to O(n).
+            */
+
+            return marks;
+        }
+
+        public static string StringReverse(string s)
+        {
+            /*Q2: Rocky is not aware of the inbuilt functions to split and reverse a string. He is given a string 
+             * and he needs to reverse the order of characters in each word within a sentence while still preserving 
+             * whitespace and initial word order. He is not allowed to use any predefined split and reverse function. 
+             * He is requesting you to complete the method for him. */
+            return s;
+        }
+
+        public static int minSum(int[] arr)
+        {
+            /*Q3: Professor Stablein is given a sorted integer array. He needs to make the array elements distinct 
+             * by increasing each value as needed, while minimizing the array sum. Professor Stablein thought this 
+             * was an interesting exercise that the students might enjoy completing. Your job is to complete the method 
+             * to print the minimum possible sum as output.*/
+            return arr[0];
+        }
+
+        public static int[] Intersect1(int[] nums1, int[] nums2)
+        {
+            /*Q4: You are given a string and your task is to sort the given string in decreasing order of frequency 
+             * of occurrence of each character.*/
+            return nums2;
+        }
+
+        public bool ContainsDuplicate(char[] arr, int k)
+        {
+            /*Q5: Rocky the Bull is new to programming and is having trouble understating the importance of time c
+             * omplexity. Professor Agrawal assigned you the job of explaining time complexity to Rocky with the e
+             * xample below.  Given two arrays, write a function to compute their intersection. */
+            return true;
+        }
+
     }
 }
+
+
+
